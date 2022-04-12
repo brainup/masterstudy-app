a53b40457ac9da4adcf66891b7084821a6fa7cb6
João Vitor <contato.cavalcantebarbosa@gmail.com>
2022-04-11 21:37:38 -0300

    failed app

---
+++ android/app/src/main/kotlin/com/brainup/brainupdevelopers_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.brainup.brainupdevelopers_app
+
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class qMainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
